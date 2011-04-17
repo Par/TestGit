b9ed33856dc5344e7ad448577298c94b5c2c7eeb
Francis Opore <francisopore@gmail.com>
2011-04-16 22:51:12 -0400

    Test commit. Let's see what happens!

--- TestGit/src/com/test/TestGit.java
+++ TestGit/src/com/test/TestGit.java
@@ -2,12 +2,32 @@ package com.test;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class TestGit extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class TestGit extends Activity implements OnClickListener
+{
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		View playVideoButton = findViewById(R.id.clickMe);
+		playVideoButton.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View view)
+	{
+		switch (view.getId())
+		{
+		case R.id.clickMe:
+		{
+			finish();
+			break;
+		}
+		}
+	}
 }
